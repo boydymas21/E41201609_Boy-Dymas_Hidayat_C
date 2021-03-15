afbedc885a2013f6d1936957a87c3bd1e1513e39
Boy Dymas Hidayat <80624528+boydymas21@users.noreply.github.com>
2021-03-15 15:42:45 +0700

    TUGAS MINGGU 2

---
+++ minggu 2/src/minggu/pkg2/TUGAS_A.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu.pkg2;
+
+/**
+ *
+ * @author USER
+ */
+public class TUGAS_A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String fName = "Boy";
+        String lName = "Dymas";
+        String stuID = "E41201609";
+        String stuStatus = "Active";
+        
+            System.out.println("Student Name\t: " + fName +" "+ lName);
+            System.out.println("Student ID\t: " + stuID);
+            System.out.println("Student Status\t: " + stuStatus);
+    }
+    
+}
